5bbbdbef55a13b2278e8cb0d5a97aff8660f9f4a
Chirag Chavada <chiragsinh1995@gmail.com>
2019-03-05 21:12:01 -0500

    Added sample footer text

--- footer.php
+++ footer.php
@@ -14,6 +14,9 @@
 
 	</div><!-- #content -->
 </div><!-- #page -->
+
+  <?php echo 'Footer Text Sample'; ?>
+
 	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
