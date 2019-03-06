fbcf66226acc431bd29a63873bbda56e0e1b620c
Aval <avaljoshi25a@gmail.com>
2019-03-05 21:03:40 -0500

    Initializing theme

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package financial_empowerment
+ */
+
+?>
+
+
+	</div><!-- #content -->
+</div><!-- #page -->
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package financial_empowerment
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'financial_empowerment' ); ?></a>
+
+
+
+	<header id="masthead" class="site-header">
+
+<div class="main_nav">
+	<div id="logo_mian">
+
+	<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$financial_empowerment_description = get_bloginfo( 'description', 'display' );
+			if ( $financial_empowerment_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $financial_empowerment_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+
+	</div>
+
+	<div id="menu_main">
+
+	<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'financial_empowerment' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</div>
+		
+			</div>
+
+
+			
+<div id="durham_tax">
+
+<h1>Durham Tax <span>Hub</span></h1>
+<p>It's never too late to file your taxes! Tax filing can boost your income, and we can show you how.</p>
+
+<p>Enter your yearly income below to see if you are eligible for free tax filing</p>
+
+
+<input type="text" class="textbox" placeholder="Income">
+
+<button class="button"><span>Am I eligible?</span></button>
+
+<div class="arrow_main">
+<div class="arrow bounce">
+  <a class="fa fa-chevron-down fa-2x" href="#"></a>
+</div>
+
+
+</div>
+			</div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
