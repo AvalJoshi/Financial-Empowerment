3becfeb108c7748b1fabe5e73b6e28b4baf28a05
Aval <avaljoshi25a@gmail.com>
2019-03-11 18:11:12 -0400

    removing the created in front.php and making get_field to the_field

--- front-page.php
+++ front-page.php
@@ -15,117 +15,19 @@
 get_header();
 ?>
 
-<<<<<<< HEAD
+<!-- <<<<<<< HEAD -->
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
 
-<<<<<<< HEAD
-=======
+<!-- <<<<<<< HEAD -->
 
-<!-- >>>>>>> master
-            <section id="howDoIqualify">
-                <h2> <?php the_field('home_page_header_text')?></h2>
-
-                <div class="qualifyDetails">
-
-                    <div class="container">
-
-                        <ul class="nav nav-tabs">
-                            <li class="active"><a data-toggle="tab" href="#home">Am I Eligible!</a></li>
-                            <li><a data-toggle="tab" href="#menu1">Receipts</a></li>
-                            <li><a data-toggle="tab" href="#menu2">Information Slips</a></li>
-
-                        </ul>
-
-                        <div class="tab-content">
-                            <div id="home" class="tab-pane fade in active">
-                                <h3><?php the_field ( 'h3_eligible')?></h3>
-                                <p>
-                                    <?php the_field('eligible_desc') ?>
-                                </p>
-                            </div>
-                            <div id="menu1 more" class="tab-pane fade">
-                                <h3><?php the_field ( 'h3_receipts')?></h3>
-                                <p>
-                                    <?php the_field('receipts_desc') ?>
-                                </p>
-                            </div>
-                            <div id="menu2" class="tab-pane fade">
-                                <h3><?php the_field ( 'h3_nformation_Slips')?></h3>
-                                <p>
-                                    <?php the_field('information_slips_desc') ?>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-
-
-
-                </div>
-            </section>
-
-            <section id="locations">
-                <h2>  <?php the_field('locations_header')?></h2>
-
-                <div class="map">
-
-                    <div class="mapouter">
-                        <div class="gmap_canvas">
-                            <?php the_field('location_area')?>
-
-                                <a href="https://www.jetzt-drucken-lassen.de"></a>
-                        </div>
-                        
-                    </div>
-
-                </div>
-            </section>
-
-            <section id="beAvolunteer">
-                <h2> Be a Volunteer </h2>
-
-                <div class="beVolunteer">
-                    <div class="howToBecomeAVolunteer">
-                        <h3>How to become a Volunteer!</h3>
-
-                        <p>
-                            It’s never too late to file your income tax return! By filing you may be eligible for many benefits! It’s never too late to file your income tax return! By filing you may be eligible for many benefits! It’s never too late to file your income tax return! By filing you may be eligible for many benefits! By filing you may be eligible for many benefits!
-                        </p>
-                    </div>
-
-                    <div class="volunteerVideo">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/-RjHEhK6p1A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-
-                    <div class="howTorunTaxClinic">
-                        <h3>How to run a tax clinic</h3>
-
-                        <p>
-                            It’s never too late to file your income tax return! By filing you may be eligible for many benefits! It’s never too late to file your income tax return! By filing you may be eligible for many benefits! It’s never too late to file your income tax return! By filing you may be eligible for many benefits! By filing you may be eligible for many benefits!
-                        </p>
-                    </div>
-                </div>
-            </section>
-
-            <section id="financialEmpowerment">
-                <h2> Financial Empowerment </h2>
-
-
-
-                <div class="financialEmpowermentDetails">
-
-                    <h3>
-======= -->
 <div id="primary" class="content-area">
 		<main id="main" class="site-main">
         
             
 
 		<section id="howDoIqualify">
-			<h2> <?php get_field('home_page_header_text')?></h2>
+			<h2> <?php the_field('home_page_header_text')?></h2>
 
 			<div class="qualifyDetails">
                 
@@ -140,36 +42,31 @@ get_header();
 
   <div class="tab-content">
     <div id="home" class="tab-pane fade in active">
-      <h3><?php get_field ( 'h3_eligible')?></h3>
-      <p>	<?php get_field('eligible_desc') ?></p>
+      <h3><?php the_field ( 'h3_eligible')?></h3>
+      <p>	<?php the_field('eligible_desc') ?></p>
     </div>
     <div id="menu1" class="tab-pane fade">
-      <h3><?php get_field ( 'h3_receipts')?></h3>
-      <p>	<?php get_field('receipts_desc') ?></p>
+      <h3><?php the_field ( 'h3_receipts')?></h3>
+      <p>	<?php the_field('receipts_desc') ?></p>
     </div>
     <div id="menu2" class="tab-pane fade">
-          <h3><?php get_field ( 'h3_nformation_Slips')?></h3>
-      <p>	<?php get_field('information_slips_desc') ?></p>
+          <h3><?php the_field ( 'h3_information_Slips')?></h3>
+      <p>	<?php the_field('information_slips_desc') ?></p>
     </div>
   </div>
-</div> 
-                
-                
-                
-                
-			
+</div>
 
-			</div>
+		</div>
 		</section>
 
 		<section id="locations">
-			<h2>  <?php get_field('locations_header')?></h2>
+			<h2>  <?php the_field('locations_header')?></h2>
 
 			<div class="map">
 
 			<div class="mapouter">
 				<div class="gmap_canvas">
-				<?php get_field('location_area')?>
+				<?php the_field('location_area')?>
 					
 					<a href="https://www.jetzt-drucken-lassen.de"></a>
 				</div>
@@ -186,12 +83,12 @@ get_header();
 				<div class="howToBecomeAVolunteer">
 					<h3>How to become a volunteer</h3>
 
-					<!--<p>
+					<p>
 					It’s never too late to file your income tax return! By filing you may be eligible for
 					many benefits! It’s never too late to file your income tax return! By filing you may be eligible for
 					many benefits! It’s never too late to file your income tax return! By filing you may be eligible for
 					many benefits! By filing you may be eligible for many benefits!
-					</p>-->
+					</p>
 				</div>	
 
 				<div class="volunteerVideo">
@@ -219,8 +116,8 @@ get_header();
 			<div class="financialEmpowermentDetails">
 
 				<h3>
->>>>>>> master
-					Overview
+<!-- >>>>>>> master
+					Overview -->
 				</h3>
 
 				<p>
