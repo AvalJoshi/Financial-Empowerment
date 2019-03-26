34c34a2b61414c12ad5aa5fbfd962100fdf823df
Aval <avaljoshi25a@gmail.com>
2019-03-26 17:14:51 -0400

    logo

--- header.php
+++ header.php
@@ -76,11 +76,11 @@
 
 =======
 			 <?php
-			// if (has_custom_logo()){
-			// the_custom_logo();
-			// }else{
-			// 	echo'No Logo';
-			// }
+			if (has_custom_logo()){
+			the_custom_logo();
+			}else{
+				echo'No Logo';
+			}
 >>>>>>> Stashed changes
 			the_custom_logo();
 
